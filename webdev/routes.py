57612021e543f43571e2b815c2408d8afa5187f2
uriel zacarias <urielzacarias@gmail.com>
2019-11-02 11:43:49 -0700

    users can now search for posts from a particual author!

--- webdev/routes.py
+++ webdev/routes.py
@@ -38,6 +38,15 @@ def showBlog():
 	return render_template('blog.html', posts=posts)
 
 
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
+
 ################################################################################
 ################################################################################
 # register
