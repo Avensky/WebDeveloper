2508bcdd09027f459595d2dddf5d34d7b37ac049
uriel zacarias <urielzacarias@gmail.com>
2019-11-27 20:41:14 -0800

    updated fb login

--- webdev/static/scripts.js
+++ webdev/static/scripts.js
@@ -1,34 +1,11 @@
-function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-  console.log('statusChangeCallback');
-  console.log(response);                   // The current login status of the person.
-  if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-    testAPI();
-  } else {                                 // Not logged into your webpage or we are unable to tell.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this webpage.';
-  }
-}
-
-
-function checkLoginState() {               // Called when a person is finished with the Login Button.
-  FB.getLoginStatus(function(response) {   // See the onlogin handler
-    statusChangeCallback(response);
-  });
-}
-
-
 window.fbAsyncInit = function() {
   FB.init({
     appId      : '599583343779617',
     cookie     : true,                     // Enable cookies to allow the server to access the session.
     status     : true,
     xfbml      : true,                     // Parse social plugins on this webpage.
-    version    : 'v2.8'                    // Use this Graph API version for this call.
+    version    : 'v5.0'                    // Use this Graph API version for this call.
   });
-  FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-    statusChangeCallback(response);        // Returns the login status.
-  });
-
 };
 
 
@@ -42,7 +19,7 @@ window.fbAsyncInit = function() {
 }(document, 'script', 'facebook-jssdk'));
 
 // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-function testAPI() {
+function sendTokenToServer() {
   var access_token = FB.getAuthResponse()['accessToken'];
   console.log(access_token)
   console.log('Welcome!  Fetching your information.... ');
