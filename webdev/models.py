35d30e42f5e384b78dcebd0bec2863976b548641
uriel zacarias <urielzacarias@gmail.com>
2019-12-04 20:57:01 -0800

    fixed user_id bug in db

--- webdev/models.py
+++ webdev/models.py
@@ -56,7 +56,7 @@ class Post(db.Model):
 	title = db.Column(db.String(100), nullable=False)
 	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow())
 	content = db.Column(db.Text, nullable=False)
-	user_id = db.Column(db.String(120), db.ForeignKey('user.id'), nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
 	def __repr__(self):
